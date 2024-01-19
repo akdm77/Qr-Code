842fedd2198c53fa0d302b0627addb759b4f6a61
alanklysmandiasmacedo <alanklysmandiasmacedo@pop-os.localdomain>
2024-01-19 17:23:15 -0300

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-200">
       <div className="bg-white zb-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex w-60">
         <div className='imagem'></div>
-        <img  src="../" alt="QR Code" />
+        <img  src="../app" alt="QR Code" />
         <h1>Improve your front-end skills by building projects</h1>
         <h6>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h6>
         
